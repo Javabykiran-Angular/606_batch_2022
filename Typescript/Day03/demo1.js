1139c8e4d92b8646729ee315ba0ef31e46dca9ca
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2022-06-30 09:31:05 +0530

    Day03

---
+++ Typescript/Day03/demo1.js
@@ -0,0 +1,2 @@
+console.log("Hello World....");
+console.log('U r learn Angular 8 Hope so u r enjoying');
---
+++ Typescript/Day03/demo1.ts
@@ -0,0 +1,5 @@
+
+console.log("Hello World....");
+console.log('U r learn Angular 8 Hope so u r enjoying');
+var a1;
+var b1,b2;
