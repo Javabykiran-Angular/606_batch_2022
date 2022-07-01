ef8e9e0d750e47fa56627e67798892d686429430
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2022-07-01 09:25:39 +0530

    Day04

---
+++ Typescript/Day04/demo1.js
@@ -0,0 +1,68 @@
+//data types
+// number(int,long,short,double,float),string('Sumit'/""),boolean(true/false)
+// any(number,string,boolean) ,void (netural data type => it is does not point any thing)
+// misec. => undefined & null => bcz it act as data type as well as value 
+//unknown,Object
+var a = 10; //forward declaration & defination
+// console.log('Value of a is '+a);
+var a1;
+// console.log("Value of a1 is "+a1);
+var str = 'Sumit Raokhande';
+//back tick operator
+// console.log(`Value of str is ${str} `);
+var a2;
+a2 = 'Sumit';
+// console.log('Value of a2 is '+a2);
+a2 = 2.5;
+// console.log('Value of a2 is '+a2);
+a2 = true;
+// console.log('Value of a2 is '+a2);
+var b1 = 45;
+// b1='sumit';
+//type assertion
+//1 angle bracket
+//2 as syntax
+// 1 angle bracket
+var b2;
+var temp = b2;
+// temp.
+//2 as syntax
+var temp2 = b2;
+// temp2.
+//Operators
+// Airthamatic => +,-,*,/,%
+// bitwise  => &,|,!,^,~,>>,<<
+// logical => &&,||
+// relatinal => !=,<=,>=,==(it only checks the value),===(exactly equal/Strongly equality it checks value as well as data type e.g  a===b)
+// assignament=> =,+=,-=,*=./=
+//ternary => condition?expression1:expression2
+//unaray => post/pre inc/dec ++a;--a,a++,a--
+//conditional
+// Control statement
+// if,if-else,nested if-else,break,continue,switch
+// var count1=21;
+//   if(count1<4){
+//     // console.log(`Condition is True`)
+//   }else{
+//     // console.log(`Condition is False`)
+//   }
+var choice = 21;
+// switch(choice){
+//   case 1: console.log(`U r in case 1`);
+//           break;
+//   case 2 :console.log(`U r in case 2`)
+//           break;
+//   default: console.log(`U r in Default case`)
+//           break;
+// }
+//Loop Statement 
+//While loop,do-while,for,foreach
+var count = 5;
+// while(count!=0){
+//   console.log("Count is "+count);
+//   count--;
+// }
+do {
+    console.log("Count is==>>  " + count);
+    count--;
+} while (count != 0);
